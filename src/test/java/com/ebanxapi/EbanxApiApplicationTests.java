267394c59a31f7640a0257ef9f8887b2d50e0383
caiopapai <caio_papai@hotmail.com>
2021-09-19 20:08:31 -0300

    Setting up Java, Spring and BDD

---
+++ src/test/java/com/ebanxapi/EbanxApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ebanxapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbanxApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
